697cd40ea02a6868620e0a04a9dedd942d830c7f
ricardoggz <ricardo_ggzicm@outlook.com>
2022-10-28 22:53:11 -0500

    Add new endpoint

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getAdmins, postAdmin, putAdmin, deleteAdmin } from "../controllers/admins.js"
+import { getAdmins, postAdmin, putAdmin, deleteAdmin, adminLogin } from "../controllers/admins.js"
 
 export const adminRouter = Router()
 //routes
 adminRouter.get('/admins', getAdmins)
+adminRouter.post('/admin-login', adminLogin)
 adminRouter.post('/insert-admin', postAdmin)
 adminRouter.put('/update-admin/:id', putAdmin)
 adminRouter.delete('/delete-admin', deleteAdmin)
