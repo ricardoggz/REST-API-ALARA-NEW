91cf43e59747829a88bd802d052cea12567507ed
ricardoggz <ricardo_ggzicm@outlook.com>
2022-10-30 21:43:06 -0600

    Add new routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,16 @@
 import { Router } from "express"
-import { userLogin } from "../controllers/users.js"
+import {
+    userLogin,
+    getUsers,
+    postUser,
+    putUser,
+    deleteUser
+} from "../controllers/users.js"
 
 export const userRouter = Router()
 //routes
+userRouter.get('/users', getUsers)
 userRouter.post('/user-login', userLogin)
+userRouter.post('/insert-user', postUser)
+userRouter.put('/update-login/:id', putUser)
+userRouter.delete('/delete-user', deleteUser)
