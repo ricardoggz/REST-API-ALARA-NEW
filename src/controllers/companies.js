1343f8b7ea911ebc47b328163356f299f2965b96
ricardoggz <ricardo_ggzicm@outlook.com>
2022-11-08 10:00:59 -0600

    Add new routes and controllers

--- src/controllers/companies.js
+++ src/controllers/companies.js
@@ -9,4 +9,46 @@ const getCompanies = (req, res)=>database.config.query(
     (err, companies) => !err ? res.json({companies}) : res.json({err})
 )
 
-export { getCompanies }
+const postCompany=(req, res)=>database.config.query(
+    `INSERT INTO razonsocial(
+        RAZON_ID,
+        RAZON_NOMBRE
+    )
+     VALUES(
+        ${req.body.RAZON_ID},
+        "${req.body.RAZON_NOMBRE}"
+     )`,
+    (err)=> !err ? res.json({
+                ok: true,
+                message: 'Datos almacenados con éxito'
+            })
+            : res.json({err})
+)
+
+const putCompany= (req, res)=> database.config.query(
+    `UPDATE razonsocial
+     SET
+     RAZON_ID = ${req.body.RAZON_ID},
+     RAZON_NOMBRE = "${req.body.RAZON_NOMBRE}"
+     WHERE RAZON_NOMBRE = "${req.params.id}"`,
+    (err)=> !err ? res.json({
+                ok: true,
+                mssage: 'Datos actualizados con éxito'
+            })
+            : res.json({err})
+)
+
+const deleteCompany= (req, res)=> database.config.query(
+    `DELETE FROM razonsocial
+     WHERE
+     RAZON_NOMBRE = "${req.body.RAZON_NOMBRE}"
+    `,
+    (err)=> !err ? res.json({
+                ok: true,
+                message: "Datos eliminados" 
+            })
+            : res.json({err})
+
+)
+
+export { getCompanies, postCompany, putCompany, deleteCompany }
--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,6 +1,14 @@
 import { Router } from "express"
-import { getCompanies } from "../controllers/companies.js"
+import {
+    getCompanies,
+    postCompany,
+    putCompany,
+    deleteCompany
+} from "../controllers/companies.js"
 
 export const companyRouter = Router()
 //routes
 companyRouter.get('/companies', getCompanies)
+companyRouter.post('/insert-company', postCompany)
+companyRouter.put('/update-company/:id', putCompany)
+companyRouter.delete('/delete-company', deleteCompany)
