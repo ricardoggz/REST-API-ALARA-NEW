eb0df7958246b29cce74cc46f595db9025102d66
ricardoggz <ricardo_ggzicm@outlook.com>
2022-11-10 19:25:05 -0600

    Change in admin controller

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -1,6 +1,23 @@
 import { database } from "../database/database.js"
 
-const getAdmins=(req, res)=>database.config.query(
+const getAdmins =(req, res)=>{
+    database.config.getConnection((err, conn)=>{
+        if(err) throw new Error(err)
+        conn.query(
+            `SELECT
+             ADMIN_ID AS ID,
+             ADMIN_NOMBRE AS NOMBRE,
+             ADMIN_USUARIO AS USUARIO
+             FROM admins
+            `,
+            (err, admins)=>{
+                !err ? res.json({admins}) : res.json({err})
+                conn.release()
+            }
+        )
+    })
+}
+/*const getAdmins=(req, res)=>database.config.query(
     `SELECT
      ADMIN_ID AS ID,
      ADMIN_NOMBRE AS NOMBRE,
@@ -8,9 +25,36 @@ const getAdmins=(req, res)=>database.config.query(
      FROM admins
      `,
     (err, admins)=> !err ? res.json({admins}) : res.json({err})
-)
+)*/
 
-const postAdmin=(req, res)=>database.config.query(
+const postAdmin = (req, res)=>{
+    database.config.getConnection((err, conn)=>{
+        if(err) throw new Error(err)
+        conn.query(
+            `INSERT INTO admins(
+                ADMIN_ID,
+                ADMIN_NOMBRE,
+                ADMIN_USUARIO,
+                ADMIN_CONTRASENA
+            )
+             VALUES(
+                ${req.body.ADMIN_ID},
+                "${req.body.ADMIN_NOMBRE}",
+                "${req.body.ADMIN_USUARIO}",
+                "${req.body.ADMIN_CONTRASENA}"
+             )`,
+             (err)=> {
+                !err ? res.json({
+                ok: true,
+                message: 'Datos almacenados con éxito'
+                })
+                : res.json({err})
+                conn.release()
+            }
+        )
+    })
+}
+/*const postAdmin=(req, res)=>database.config.query(
     `INSERT INTO admins(
         ADMIN_ID,
         ADMIN_NOMBRE,
@@ -28,7 +72,7 @@ const postAdmin=(req, res)=>database.config.query(
                 message: 'Datos almacenados con éxito'
             })
             : res.json({err})
-)
+)*/
 
 const putAdmin= (req, res)=> database.config.query(
     `UPDATE admins
